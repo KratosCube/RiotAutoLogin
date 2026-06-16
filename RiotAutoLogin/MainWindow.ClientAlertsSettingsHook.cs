434e306abb6026a5c35901d44dc2bc4a6d9eedbf
Václav Chuchlík <106406265+KratosCube@users.noreply.github.com>
2026-06-17 00:44:53 +0200

    Update static client alert toggle text

--- RiotAutoLogin/MainWindow.ClientAlertsSettingsHook.cs
+++ RiotAutoLogin/MainWindow.ClientAlertsSettingsHook.cs
@@ -1,10 +1,15 @@
 using System;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
 using System.Windows.Threading;
 
 namespace RiotAutoLogin
 {
     public partial class MainWindow
     {
+        private bool _staticClientAlertControlsHooked;
+        private bool _suppressStaticClientAlertEvents;
+
         protected override void OnInitialized(EventArgs e)
         {
             base.OnInitialized(e);
@@ -24,6 +29,9 @@ namespace RiotAutoLogin
             {
                 attempts++;
 
+                HookStaticClientAlertSettingsControls();
+                UpdateStaticClientAlertSettingsUi();
+
                 EnsureClientAlertSettingsCard();
                 UpdateClientAlertSettingsUi();
 
@@ -32,6 +40,113 @@ namespace RiotAutoLogin
             };
 
             timer.Start();
+        }
+
+        private void HookStaticClientAlertSettingsControls()
+        {
+            if (_staticClientAlertControlsHooked)
+                return;
+
+            if (FindName("tglGameStartAlert") is not ToggleButton gameStartToggle ||
+                FindName("tglFlashSlotWarning") is not ToggleButton flashWarningToggle ||
+                FindName("rbFlashSlot1") is not RadioButton flashSlot1 ||
+                FindName("rbFlashSlot2") is not RadioButton flashSlot2)
+            {
+                return;
+            }
+
+            _staticClientAlertControlsHooked = true;
+
+            gameStartToggle.Checked += (_, _) =>
+            {
+                if (_suppressStaticClientAlertEvents)
+                    return;
+
+                _hotkeySettings.GameStartAlertEnabled = true;
+                SaveHotkeySettings();
+                UpdateStaticClientAlertSettingsUi();
+            };
+
+            gameStartToggle.Unchecked += (_, _) =>
+            {
+                if (_suppressStaticClientAlertEvents)
+                    return;
+
+                _hotkeySettings.GameStartAlertEnabled = false;
+                SaveHotkeySettings();
+                UpdateStaticClientAlertSettingsUi();
+            };
+
+            flashWarningToggle.Checked += (_, _) =>
+            {
+                if (_suppressStaticClientAlertEvents)
+                    return;
+
+                _hotkeySettings.FlashSlotWarningEnabled = true;
+                SaveHotkeySettings();
+                UpdateStaticClientAlertSettingsUi();
+            };
+
+            flashWarningToggle.Unchecked += (_, _) =>
+            {
+                if (_suppressStaticClientAlertEvents)
+                    return;
+
+                _hotkeySettings.FlashSlotWarningEnabled = false;
+                SaveHotkeySettings();
+                UpdateStaticClientAlertSettingsUi();
+            };
+
+            flashSlot1.Checked += (_, _) =>
+            {
+                if (_suppressStaticClientAlertEvents)
+                    return;
+
+                _hotkeySettings.PreferredFlashSlot = 1;
+                SaveHotkeySettings();
+                UpdateStaticClientAlertSettingsUi();
+            };
+
+            flashSlot2.Checked += (_, _) =>
+            {
+                if (_suppressStaticClientAlertEvents)
+                    return;
+
+                _hotkeySettings.PreferredFlashSlot = 2;
+                SaveHotkeySettings();
+                UpdateStaticClientAlertSettingsUi();
+            };
+        }
+
+        private void UpdateStaticClientAlertSettingsUi()
+        {
+            if (FindName("tglGameStartAlert") is not ToggleButton gameStartToggle ||
+                FindName("tglFlashSlotWarning") is not ToggleButton flashWarningToggle ||
+                FindName("rbFlashSlot1") is not RadioButton flashSlot1 ||
+                FindName("rbFlashSlot2") is not RadioButton flashSlot2)
+            {
+                return;
+            }
+
+            if (_hotkeySettings.PreferredFlashSlot != 1 && _hotkeySettings.PreferredFlashSlot != 2)
+                _hotkeySettings.PreferredFlashSlot = 2;
+
+            _suppressStaticClientAlertEvents = true;
+            try
+            {
+                gameStartToggle.IsChecked = _hotkeySettings.GameStartAlertEnabled;
+                gameStartToggle.Content = _hotkeySettings.GameStartAlertEnabled ? "ON" : "OFF";
+
+                flashWarningToggle.IsChecked = _hotkeySettings.FlashSlotWarningEnabled;
+                flashWarningToggle.Content = _hotkeySettings.FlashSlotWarningEnabled ? "ON" : "OFF";
+
+                flashSlot1.IsChecked = _hotkeySettings.PreferredFlashSlot == 1;
+                flashSlot2.IsChecked = _hotkeySettings.PreferredFlashSlot == 2;
+            }
+            finally
+            {
+                _suppressStaticClientAlertEvents = false;
+            }
         }
     }
 }
